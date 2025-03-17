8ed977ec5dfa81d2c963a52789d735fd9c0e5a7a
mgenge254 <jeremiah.orodi@student.moringaschool.com>
2025-03-17 09:56:07 +0300

    new commit

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
+# teacher.py
 from user import User
-
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = ["Math", "Science", "History", "Programming"]
 
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
